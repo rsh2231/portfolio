ebe6656c5c87ec8c138fc66d21f29744ac564c8f
rsh2231 <rsh2231@naver.com>
2025-08-28 11:56:13 +0900

    v12

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // GitHub Pages에서 서브디렉토리에 호스팅하는 경우 basePath를 설정해야 합니다.
-  // 예: https://<username>.github.io/<repository-name>
-  // 이 경우, basePath: '/<repository-name>',
-  // assetPrefix: '/<repository-name>/' 와 같이 설정합니다.
-  // 현재 프로젝트는 shryu.github.io 이므로, basePath는 필요하지 않습니다.
+  basePath: '/shryu.github.io',
+  assetPrefix: '/shryu.github.io/',
 };
 
 export default nextConfig;
